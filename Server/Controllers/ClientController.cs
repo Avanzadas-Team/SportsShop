2b4ef43ee8c79db3f3eabadfc66652fcfb896df8
Gabriel Abarca <gababarca@hotmail.com>
2020-12-04 06:20:37 -0600

    [Add] Some Models

--- Server/Controllers/ClientController.cs
+++ Server/Controllers/ClientController.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Persistence;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Server.Controllers
 {
@@ -11,6 +10,28 @@ namespace Server.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
-        
+        private readonly GraphDbContext _graphContext;
+        public ClientController(GraphDbContext graphDbContext)
+        {
+            _graphContext = graphDbContext;
+        }
+        [HttpPut]
+        public async Task<IActionResult> Register(User user)
+        {
+            return Ok(user);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Buy(User user, Article article)
+        {
+            return Ok(article);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> History(User user)
+        {
+            IEnumerable<Article> articlesBought = new List<Article>();
+            _graphContext
+            return Ok(articlesBought);
+        }
     }
 }
---
+++ Server/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Server.Models
+{
+    public class Article : INode
+    {
+        public string Id { get; set; }
+        public string Name { get; set ; }
+        public string Make { get; set; }
+        public decimal Price { get; set; }
+        public IEnumerable<string> sports { get; set; }
+        public bool limitEd { get; set; }
+        public ArticleType Type { get; set; }
+        public string ImageId { get; set; }
+    }
+    public enum ArticleType
+    {
+        Clothe, Article
+    }
+}
