633d08c7d3d89967a7e6b00ac663c93ef53ed772
kevin9907 <kevin0607as@gmail.com>
2020-12-07 15:21:06 -0600

    [update] bought

--- Server/Models/Bought.cs
+++ Server/Models/Bought.cs
@@ -7,6 +7,7 @@ namespace Server.Models
 {
     public class Bought
     {
+        public string clientId { get; set; }
         public IEnumerable<Cart> Cart { get; set; }
         DateTime Date { get; set; }
     }
