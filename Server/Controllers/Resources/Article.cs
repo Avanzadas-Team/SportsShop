a6592a80f024f15eb1f88bdd775734dd1a24b885
kgonzalez712 <kevin.gonzalezs0795@gmail.com>
2020-12-11 16:22:14 -0600

    [Update] User friendly date format

--- Server/Controllers/Resources/Article.cs
+++ Server/Controllers/Resources/Article.cs
@@ -5,10 +5,10 @@ namespace Server.Controllers.Resources
 {
     public class Article
     {
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public Models.ProductMDB Product { get; set; }
 
-        public Article(DateTime date, ProductMDB article)
+        public Article(string date, ProductMDB article)
         {
             Date = date;
             this.Product = article;
